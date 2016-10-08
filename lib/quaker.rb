ba459a0990e7dbc4f9d3fba5e7c8977ea8ffef89
Igor Shapiro <shapigor@gmail.com>
2016-10-08 16:48:48 +0300

    Added --dir option Added build directory resolving by git repo

--- lib/quaker.rb
+++ lib/quaker.rb
@@ -1,6 +1,7 @@
 require "quaker/version"
 require "quaker/include"
 require "quaker/tag_filter"
+require "quaker/git_resolver"
 require "quaker/compose_file"
 
 module Quaker
@@ -8,17 +9,30 @@ module Quaker
 
   Clamp do
     parameter "[SPEC_FILE]", "Extended docker-compose file"
-    option %w(--tags -t), "TAGS", "tags", multivalued: true
+    option %w(--tags -t), "TAGS", "Filter services (and dependencies) by tag", multivalued: true
+    option %w(--dir -d), "DIR", "Specify base directory"
 
     def default_spec_file
-      File.expand_path('docker/services/all.yml', Dir.pwd)
+      File.expand_path('docker/services/all.yml', dir)
     end
 
+    def default_dir
+      Dir.pwd
+    end
+
+    def dump_params
+      puts "Spec: #{spec_file}"
+      puts "Dir: #{dir}"
+    end
 
     def execute
-      puts "Using #{spec_file}"
+      dump_params
+
+      Dir.chdir dir
+
       spec = Include.new.process spec_file
       spec = TagFilter.new.filter spec, tags_list
+      spec = GitResolver.new.resolve spec
       spec = ComposeFile.new.build spec
       puts spec
     end
---
+++ lib/quaker/git_resolver.rb
@@ -0,0 +1,31 @@
+require 'open3'
+
+class GitResolver
+  def find_dir_for_repo repo
+    dir = Dir.glob('*')
+      .select {|f| File.directory? f}
+      .select {|dir|
+        stdin, stdout, stderr = Open3.popen3("cd #{dir} && git remote -v")
+        out = stdout.gets
+        out && out.include?(repo)
+      }
+      .first
+    return nil unless dir
+    "./#{dir}"
+  end
+
+  def resolve services_map
+    for name, spec in services_map
+      puts spec
+      git_repo = spec.delete("git")
+      next unless git_repo
+
+      dir = find_dir_for_repo git_repo
+
+      puts "ERROR: Unable to find dir for repo #{git_repo}" and return unless dir
+
+      spec["build"] = dir
+    end
+    services_map
+  end
+end
